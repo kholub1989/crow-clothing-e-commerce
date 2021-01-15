3307a29389cf4125e086e5769ebb7d7219420f3d
Krisztian Holub <kholub1989@gmail.com>
2021-01-14 19:24:14 -0500

    create sing up with email and password, fix typos

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ class App extends React.Component {
               ...snapShot.data(),
             },
           });
+          console.log(this.state);
         });
       }
       this.setState({
--- src/components/sing-up/sing-up.component.jsx
+++ src/components/sing-up/sing-up.component.jsx
@@ -42,7 +42,7 @@ class SignUp extends React.Component {
         confirmPassword: "",
       });
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   };
 
@@ -58,7 +58,7 @@ class SignUp extends React.Component {
       <div className="sign-up">
         <h2 className="title">I do not have a account</h2>
         <span>Sign up with your email and password</span>
-        <form className="sign-up-form">
+        <form className="sign-up-form" onSubmit={this.handleSubmit}>
           <FormInput
             type="text"
             name="displayName"
