ee60627ab8504c4466909a5738cbf902592d0037
Krisztian Holub <kholub1989@gmail.com>
2021-01-16 12:07:10 -0500

    fix typo in commponent name

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,7 +5,7 @@ import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
-import "./card-icon.styles.scss";
+import "./cart-icon.styles.scss";
 
 const CartIcon = ({ toggleCartHidden }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
 import { auth } from "../../firebase/firebase.utils";
-import CartIcon from "../card-icon/card-icon.component";
+import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
