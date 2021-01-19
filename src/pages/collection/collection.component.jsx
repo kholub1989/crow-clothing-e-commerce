9727933e5f971a36b8d15245c21307b00cb2b3d8
Krisztian Holub <kholub1989@gmail.com>
2021-01-19 15:33:35 -0500

    add collection routing and selector

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import { connect } from "react-redux";
 
-import CollectionItem from "../../components/collection-item/collection-item.component";
+// import CollectionItem from "../../components/collection-item/collection-item.component";
+
+import { selectCollection } from "../../redux/shop/shop.selectors";
 
 import "./collection.styles.scss";
 
-const CollectionPage = ({ match }) => (
-  <div className="collection-page">
-    <h2>Collection PAGE</h2>
-  </div>
-);
+const CollectionPage = ({ collection }) => {
+  console.log("collection: ", collection);
 
-export default CollectionPage;
+  return (
+    <div className="collection-page">
+      <h2>Collection PAGE</h2>
+    </div>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
